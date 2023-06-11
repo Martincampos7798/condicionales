7202f62c77f36c46bdbfa42a1f6507c2b06f1961
Martincampos7798 <martincampospaez8609@gmail.com>
2023-06-10 19:52:13 -0500

    primer ejercicio condicional

---
+++ 1.1condicionales/script.js
@@ -0,0 +1,9 @@
+
+
+let belleza = prompt ("eres bellisimo");
+
+if ( belleza == ("si")) {
+    alert ("ciertamente");
+} else {
+   alert ("no te creo") ;
+}
---
+++ 1.2condicionales/script.js
@@ -0,0 +1,11 @@
+
+
+let numero = prompt ("introduce un numero")
+
+
+if ( numero %2 == 0)  {
+    alert ("es divisible entre dos")
+} else {
+    alert ("no es divisible entre dos")
+}
+
